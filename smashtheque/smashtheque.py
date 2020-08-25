9f3199258262b32e1fcccb4bc691e0b6b36f32e9
leElvyn <redking705@gmail.com>
2020-08-25 18:37:33 +0200

    formatage avec black

--- smashtheque/smashtheque.py
+++ smashtheque/smashtheque.py
@@ -7,30 +7,41 @@ from redbot.core.bot import Red
 from typing import Optional
 import re
 import json
+
 urls = {
     "characters": "https://retropen-base.herokuapp.com/api/v1/characters",
     "teams": "https://retropen-base.herokuapp.com/api/v1/teams",
     "cities": "https://retropen-base.herokuapp.com/api/v1/cities",
-    "players": "https://retropen-base.herokuapp.com/api/v1/players"
-
+    "players": "https://retropen-base.herokuapp.com/api/v1/players",
 }
+
+
 async def yeet(ctx, erreur):
     """lever des erreurs"""
-    embed = discord.Embed(title=f"Erreur dans la commande {ctx.command} :", description=erreur, colour=discord.Colour(0xd0021b))
-    embed.set_author(name="smashthèque ", icon_url="https://cdn.discordapp.com/avatars/745022618356416572/c8fa739c82cdc5a730d9bdf411a552b0.png?size=1024")
+    embed = discord.Embed(
+        title=f"Erreur dans la commande {ctx.command} :",
+        description=erreur,
+        colour=discord.Colour(0xD0021B),
+    )
+    embed.set_author(
+        name="smashthèque ",
+        icon_url="https://cdn.discordapp.com/avatars/745022618356416572/c8fa739c82cdc5a730d9bdf411a552b0.png?size=1024",
+    )
     await ctx.send(embed=embed)
+
 
 async def id_to_name(id_find, url, self):
     async with self._session.get(urls[url]) as r:
         result = await r.json()
-        non_conflict_name_list = loop_dict(result, "id", id_find) 
+        non_conflict_name_list = loop_dict(result, "id", id_find)
         return non_conflict_name_list["name"]
-        
+
 
 def loop_dict(dict_to_use, value, parameter):
     for sub in dict_to_use:
         if sub[value] == parameter:
             return sub
+
 
 def format_emojis(id_list):
     end = []
@@ -38,12 +49,15 @@ def format_emojis(id_list):
         end.append("<:placeholder:{i}>")
     return end
 
-class Smashtheque(commands.Cog):
 
+class Smashtheque(commands.Cog):
     async def initialize(self):
         bearer = await self.bot.get_shared_api_tokens("smashtheque")
         bearer = bearer["bearer"]
-        headers={"Authorization": f"Bearer {bearer}", "Content-Type": "application/json"}
+        headers = {
+            "Authorization": f"Bearer {bearer}",
+            "Content-Type": "application/json",
+        }
         self._session = aiohttp.ClientSession(headers=headers)
 
     def __init__(self, bot: Red):
@@ -52,34 +66,48 @@ class Smashtheque(commands.Cog):
     def cog_unload(self):
         asyncio.create_task(self._session.close())
 
-
     @commands.command()
     async def test3s(self, ctx):
-        async with self._session.post(urls["players"], data=json.dumps({'player': {'name': 'red', 'character_ids': [26], 'creator_discord_id': '332894758076678144', 'team_id': 18, 'city_id': 24}}), headers={'content-type': 'application/json'}) as r:
+        async with self._session.post(
+            urls["players"],
+            data=json.dumps(
+                {
+                    "player": {
+                        "name": "red",
+                        "character_ids": [26],
+                        "creator_discord_id": "332894758076678144",
+                        "team_id": 18,
+                        "city_id": 24,
+                    }
+                }
+            ),
+            headers={"content-type": "application/json"},
+        ) as r:
             print(r.status)
 
     @commands.command(usage="<pseudo> <emotes de persos> [team] [ville] [id discord]")
     @commands.is_owner()
-    async def apite(self, ctx,*, arg):
+    async def apite(self, ctx, *, arg):
         """cette commande va vous permettre d'ajouter un joueur dans la base de données de smashthèque.\n\nVous devez ajouter au minimum le pseudo et les persos joués dans l'ordre.\n\nVous pouvez aussi ajouter sa team, sa ville, et si il possède un compte discord, son id pour qu'il puisse modifier lui-même son compte.\n\nVous pouvez récupérer l'id avec les options de developpeur (activez-les dans l'onglet Apparence des paramètres de l'utilisateur, puis faites un clic droit sur l'utilisateur et sélectionnez \"Copier ID\".)\n\n\nExamples : \n- !placeholder Pixel <:Yoshi:737480513744273500> <:Bowser:747063692658737173>\n- !placeholder red <:Joker:742166628598284359> LoS Paris 332894758076678144\n"""
         print(arg)
         current_stage = 0
         alts = []
         if re.search(r"<a?:(\w+):(\d+)>", arg) == None:
-            await yeet(ctx, "Veuillez rentrer les personnages utilisés par le joueur grâce aux émojis de personnages du serveur")
+            await yeet(
+                ctx,
+                "Veuillez rentrer les personnages utilisés par le joueur grâce aux émojis de personnages du serveur",
+            )
             return
-        response = {
-            "name": "",
-            "character_ids": [],
-            "creator_discord_id": ""
-        }
+        response = {"name": "", "character_ids": [], "creator_discord_id": ""}
         x = arg.split()
         loop = 1
         for argu in x:
             if current_stage == 0:
                 if re.search(r"<a?:(\w+):(\d+)>", argu) != None:
                     if loop == 1:
-                        await yeet(ctx, "Veuillez commencer par donner le nom du joueur.")
+                        await yeet(
+                            ctx, "Veuillez commencer par donner le nom du joueur."
+                        )
                         return
                     else:
                         current_stage = 1
@@ -88,11 +116,14 @@ class Smashtheque(commands.Cog):
                             result = await r.json()
                             emoji_dict = {}
                             for sub in result:
-                                if sub['emoji'] == re.search(r"[0-9]+", argu).group():
+                                if sub["emoji"] == re.search(r"[0-9]+", argu).group():
                                     emoji_dict = sub
                                     break
                             if emoji_dict == {}:
-                                await yeet(ctx, "Veuillez utiliser les bon émojis de perso du serveur.")
+                                await yeet(
+                                    ctx,
+                                    "Veuillez utiliser les bon émojis de perso du serveur.",
+                                )
                                 return
                             response["character_ids"].append(emoji_dict["id"])
                             continue
@@ -106,11 +137,13 @@ class Smashtheque(commands.Cog):
                     continue
                 else:
                     for sub in result:
-                        if sub['emoji'] == re.search(r"[0-9]+", argu).group():
+                        if sub["emoji"] == re.search(r"[0-9]+", argu).group():
                             emoji_dict = sub
                             break
                     if emoji_dict == {}:
-                        await yeet(ctx, "Veuillez utiliser les bon émojis de perso du serveur.")
+                        await yeet(
+                            ctx, "Veuillez utiliser les bon émojis de perso du serveur."
+                        )
                         return
                     response["character_ids"].append(emoji_dict["id"])
                     continue
@@ -132,7 +165,10 @@ class Smashtheque(commands.Cog):
                                     response["city_id"] = i["id"]
                                     break
                         if "city_id" not in response:
-                            await yeet(ctx, "Veuillez entrer un nom de team correct.\nPour ça, utilisez son tag.\nExample : `LoS` ou `RB`")
+                            await yeet(
+                                ctx,
+                                "Veuillez entrer un nom de team correct.\nPour ça, utilisez son tag.\nExample : `LoS` ou `RB`",
+                            )
                             return
                         else:
                             current_stage = 4
@@ -150,7 +186,10 @@ class Smashtheque(commands.Cog):
                             response["city_id"] = i["id"]
                             break
                     if "city_id" not in response:
-                        await yeet(ctx, "Veuillez entrer un nom de ville correct.\nSi votre ville n'éxiste pas encore, demandez a un admin de l'ajouter.")
+                        await yeet(
+                            ctx,
+                            "Veuillez entrer un nom de ville correct.\nSi votre ville n'éxiste pas encore, demandez a un admin de l'ajouter.",
+                        )
                         return
                 current_stage = 4
                 continue
@@ -158,7 +197,10 @@ class Smashtheque(commands.Cog):
                 if argu.isdigit() == True and len(str(argu)) == 18:
                     response["discord_id"] = str(argu)
                 else:
-                    await yeet(ctx, "veuillez entrer l'id discord du joueur à ajouter.\nPour avoir son ID, activez simplement les options de développeur dans l'onglet apparence de discord, puis faites clic droit sur l'utilisateur > copier l'id.")
+                    await yeet(
+                        ctx,
+                        "veuillez entrer l'id discord du joueur à ajouter.\nPour avoir son ID, activez simplement les options de développeur dans l'onglet apparence de discord, puis faites clic droit sur l'utilisateur > copier l'id.",
+                    )
                     return
             loop += 1
         async with self._session.get(urls["players"]) as r:
@@ -167,7 +209,10 @@ class Smashtheque(commands.Cog):
             alts.append(loop_dict(result, "name", response["name"]))
             print(alts)
             if alts != [None]:
-                embed = discord.Embed(title="une ou plusieurs personnes possèdent le même pseudo que la personne que vous souhaitez ajouter. ", colour=discord.Colour(0xa54c4c))
+                embed = discord.Embed(
+                    title="une ou plusieurs personnes possèdent le même pseudo que la personne que vous souhaitez ajouter. ",
+                    colour=discord.Colour(0xA54C4C),
+                )
                 for users in alts:
                     async with self._session.get(urls["characters"]) as r:
                         result = await r.json()
@@ -176,14 +221,18 @@ class Smashtheque(commands.Cog):
                         for i in users["character_ids"]:
                             characters_result = loop_dict(result, "id", i)
                             alts_emojis.append(characters_result["emoji"])
-                    formated_player = "personnages : {0}".format(format_emojis(str(alts_emojis)))
+                    formated_player = "personnages : {0}".format(
+                        format_emojis(str(alts_emojis))
+                    )
                     alts_team = await id_to_name(users["team_id"], "teams", self)
                     if alts_team != None:
                         formated_player = formated_player + ", Team : {alts_team}"
                     alts_ville = await id_to_name(users["city_id"], "cities", self)
                     if alts_ville != None:
                         formated_player = formated_player + ", Ville : {alts_ville}"
-                    embed.add_field(name="pseudo : {0}".format(users["name"]), value=formated_player)
+                    embed.add_field(
+                        name="pseudo : {0}".format(users["name"]), value=formated_player
+                    )
                 pred = MessagePredicate.yes_or_no(ctx)
                 temp_message = await ctx.send(embed=embed)
                 try:
@@ -197,3 +246,4 @@ class Smashtheque(commands.Cog):
         response = {"player": response}
         async with self._session.post(urls["players"], json=response) as r:
             print(r)
+        print(response)
